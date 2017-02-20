e300606872315280dfccd9d8afd71f4dae935d90
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-20 12:17:50 -0600

    Add search for planets from SWAPI

--- app/index.js
+++ app/index.js
@@ -59,3 +59,53 @@ searchBtn.addEventListener('click', function () {
 searchBtn.addEventListener('click', function () {
   window.alert('This can run too!!!');
 });
+
+function makePlanet(planet) {
+  // Create a div
+  const planetEl = document.createElement('div');
+  // Add the class "result-item" to that div
+  planetEl.classList.add('result-item');
+
+  // Fill in the child elements for the "result-item"
+  planetEl.innerHTML = `
+    <h2 class="result-item__name"></h2>
+    <p class="result-item__population"></p>
+    <p class="result-item__gravity"></p>`;
+
+  // Set the innerText of the child elements based on the "planet" data
+  planetEl.querySelector('.result-item__name').innerText = planet.name;
+  planetEl.querySelector('.result-item__population').innerText = `${planet.population} Lifeforms`;
+  planetEl.querySelector('.result-item__gravity').innerText = `Gravity: ${planet.gravity}`;
+
+  return planetEl;
+}
+
+function fillResults(results) {
+  const resultListEl = document.querySelector('.results');
+  resultListEl.innerHTML = '';
+
+  results.forEach((planet) => {
+    const planetEl = makePlanet(planet);
+
+    resultListEl.appendChild(planetEl);
+  });
+}
+
+function searchForPlanet(name) {
+  // Look up a list of planets that match the search name
+  fetch(`http://swapi.co/api/planets?search=${name}`)
+    .then((res) => {
+      return res.json();
+    }).then((data) => {
+      // Fill in a list of search results
+      fillResults(data.results);
+    });
+}
+
+searchForPlanet('Yavin');
+
+// Listen for the searchBtn to be clicked
+searchBtn.addEventListener('click', () => {
+  // Run "searchForPlanet" with the input value
+  searchForPlanet(searchInput.value);
+});
